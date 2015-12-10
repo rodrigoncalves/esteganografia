ffee1aff8cbd7498e68819f66b6946fd0505122a
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-12-09 22:06:47 -0200

    Refactoring

--- main.cpp
+++ main.cpp
@@ -6,58 +6,34 @@
 #include <vector>
 #include <openssl/md5.h>
 
-FILE *outFile, *hashFile;
-std::vector<char> hash;
-char bits[8];
-bool hash_mode = 0;
+#define HASH_SIZE 32
 
+FILE *imgFile, *outFile, *keyFile, *hashFile;
+std::vector<char> hashBits;
+char stegBits[8];
+char *key;
+bool hash_mode = 0;
+int steg_w, steg_h, nbits;
+
+void init(char* argv[]);
 void getBits(int, char);
 char convertToByte(char*);
+void exit();
 
 int main(int argc, char *argv[])
 {
-    FILE *keyFile;
-    FILE *imgFile;
-    char *key;
     const int img_w = 1280, img_h = 720;
-    int steg_w, steg_h;
-    int nbits;
 
-    if (argc < 4)
+    if (argc < 6)
     {
-        printf("Usage: ./main <steg-width> <steg-height> <bits>\n");
+        printf("Usage: ./main <input-image> <key-file> <steg-width> <steg-height> <bits> <key-file> \n");
         exit(0);
     }
 
-    steg_w = atoi(argv[1]);
-    steg_h = atoi(argv[2]);
-    nbits = atoi(argv[3]);
-
-    keyFile = fopen("res/key2.txt", "r");
-    if (keyFile == NULL)
-    {
-        printf("Couldn't open res/key.txt.\n");
-        exit(0);
-    }
-
-    hashFile = fopen("hash.txt", "w+");
-    outFile = fopen("out.y", "w+");
-    fprintf(outFile, "P5 %d %d 255 ", steg_w, steg_h);
-
-    key = (char *) malloc(sizeof(char) * 100);
-    fgets(key, 100, keyFile);
-
-    imgFile = fopen("res/imagem_1280x720_imagemSteg_30x30_5bits.y", "r");
-    imgFile = fopen("res/video_1280x720_imagemSteg_1920x1080_4bits_chave1_comHash.y", "r+b");
-    imgFile = fopen("res/video_1280x720_imagemSteg_512x512_3bits_chave2_comHash.y", "r+b");
-    if (imgFile == NULL)
-    {
-        printf("Couldn't open image.\n");
-        exit(0);
-    }
+    init(argv);
 
     long k=-1, pixels=0;
-    for (int row_block=0; pixels < steg_w * steg_h + 32; row_block++)
+    for (int row_block=0; pixels < steg_w * steg_h + HASH_SIZE; row_block++)
     {
         for (int col_block=0; col_block < img_w/3; col_block++)
         {
@@ -70,17 +46,18 @@ int main(int argc, char *argv[])
             char byte = fgetc(imgFile);
             getBits(nbits, byte);
             ++pixels;
+
             if (hash_mode) {
-                int position = ftell(imgFile);
-                char new_byte = byte & 0xF8; // 4 bits
-                fseek(imgFile, position-1, SEEK_SET);
-                fwrite(&new_byte, sizeof(char), sizeof(char), imgFile);
-                fseek(imgFile, position, SEEK_SET);
+                int pos = ftell(imgFile);
+                char restoredByte = byte >> nbits << nbits;
+                fseek(imgFile, pos-1, SEEK_SET);
+                fwrite(&restoredByte, sizeof(char), sizeof(char), imgFile);
+                fseek(imgFile, pos, SEEK_SET);
             }
 
             if (pixels == steg_w * steg_h) {
                 hash_mode = 1;
-            } else if (pixels == steg_w * steg_h + 32) {
+            } else if (pixels == steg_w * steg_h + HASH_SIZE) {
                 break;
             }
         }
@@ -90,20 +67,45 @@ int main(int argc, char *argv[])
     MD5_CTX mdContext;
     int bytes;
     unsigned char data[1024];
-    unsigned char c[MD5_DIGEST_LENGTH];
+    unsigned char digest[MD5_DIGEST_LENGTH];
     MD5_Init(&mdContext);
     while ((bytes = fread (data, 1, 1024, imgFile)) != 0)
         MD5_Update(&mdContext, data, bytes);
-    MD5_Final (c, &mdContext);
-    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", c[i]);
+    MD5_Final (digest, &mdContext);
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", digest[i]);
     printf("\n");
 
-    fclose(keyFile);
-    fclose(imgFile);
-    fclose(outFile);
-    free(key);
+    exit();
 
     return 0;
+}
+
+void init(char* argv[])
+{
+    imgFile = fopen(argv[1], "r+b");
+    if (imgFile == NULL)
+    {
+        printf("Couldn't open %s.\n", argv[1]);
+        exit(0);
+    }
+
+    steg_w = atoi(argv[2]);
+    steg_h = atoi(argv[3]);
+    nbits = atoi(argv[4]);
+
+    keyFile = fopen(argv[5], "r");
+    if (keyFile == NULL)
+    {
+        printf("Couldn't open %s.\n", argv[5]);
+        exit(0);
+    }
+
+    key = (char *) malloc(sizeof(char) * 100);
+    fgets(key, 100, keyFile);
+
+    hashFile = fopen("hash.txt", "w+");
+    outFile = fopen("out.y", "w+");
+    fprintf(outFile, "P5 %d %d 255 ", steg_w, steg_h);
 }
 
 void getBits(int nbits, char byte)
@@ -112,23 +114,23 @@ void getBits(int nbits, char byte)
         for (int i = 0; i < nbits; ++i)
         {
             char bit = (byte >> i) & 0x01;
-            hash.push_back(bit);
+            hashBits.push_back(bit);
 
-            if (hash.size() == CHAR_BIT)
+            if (hashBits.size() == CHAR_BIT)
             {
-                char byte = convertToByte(&hash[0]);
+                char byte = convertToByte(&hashBits[0]);
                 fprintf(hashFile, "%c", byte);
-                hash.clear();
+                hashBits.clear();
             }
         }
     } else {
         for (int i = 0; i < nbits; ++i)
         {
             char bit = (byte >> i) & 0x01;
-            bits[i] = bit;
+            stegBits[i] = bit;
         }
-        char outByte = convertToByte(bits);
-        fprintf(outFile, "%c", outByte);
+        char stegByte = convertToByte(stegBits);
+        fprintf(outFile, "%c", stegByte);
     }
 }
 
@@ -145,4 +147,13 @@ char convertToByte(char* bits)
     }
 
     return byte;
+}
+
+void exit()
+{
+    fclose(keyFile);
+    fclose(imgFile);
+    fclose(outFile);
+    fclose(hashFile);
+    free(key);
 }
